86258582546c3caa507a74892bea1521d4a02c33
mafaz <mafaz.ahsan@gmail.com>
2022-12-30 11:30:50 +0100

    setuped typescript

--- SERVER/index.ts
+++ SERVER/index.ts
@@ -3,6 +3,6 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT ?? 4000;
 
 app.listen(PORT, () => console.log(`Server listening to port ${PORT}`));
